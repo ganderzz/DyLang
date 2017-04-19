bc264fca56e53f35dfe02f7623a4c18663f8f850
ganderzz <stolen3@gmail.com>
2017-04-18 19:34:49 -0700

    Improved error handling

--- build/dylang.js
+++ build/dylang.js
@@ -241,126 +241,151 @@ function transformer(ast) {
 }
 
 function tokenize(input) {
-  let current = 0;
   const tokens = [];
 
-  while (current < input.length) {
-    const currentElement = input[current];
+  const rows = input.split("\n");
+  let isComment = false;
 
-    if (currentElement === "#") {
-      current++;
-      if (input[current] === "#") {
+  const rowsLength = rows.length;
+  for (let i = 0; i < rowsLength; i++) {
+
+    const colLength = rows[i].length;
+    let current = 0;
+    while (current < colLength) {
+      const currentElement = rows[i][current];
+
+      if (isComment && currentElement !== "#") {
         current++;
-        while (input[current++] !== "#") {}
-      } else {
-        while (input[current++] !== "\n") {}
-      }
-
-      continue;
-    }
-
-    if (/\s/.test(currentElement)) {
-      current++;
-      continue;
-    }
-
-    if (/[0-9]/.test(currentElement)) {
-      let value = "";
-
-      while (/[0-9]/.test(input[current])) {
-        value += input[current];
+        continue;
+      } else if (isComment && currentElement === "#") {
+        isComment = false;
         current++;
+        continue;
       }
 
-      tokens.push({
-        type: "number",
-        value: parseInt(value, 10)
-      });
+      if (currentElement === "#") {
+        if (rows[i][++current] === "#") {
+          current++;
+          isComment = true;
+          continue;
+        }
 
-      continue;
-    }
+        break;
+      }
 
-    if (currentElement === "$") {
-      current++;
-
-      let variableName = "";
-
-      while (/[a-z]/i.test(input[current])) {
-        variableName += input[current];
+      if (/\s/.test(currentElement)) {
         current++;
+        continue;
       }
 
-      tokens.push({
-        type: "variable",
-        value: variableName
-      });
+      if (/[0-9]/.test(currentElement)) {
+        let value = "";
 
-      continue;
-    }
-
-    if (currentElement === "=") {
-      current++;
-
-      tokens.push({
-        type: "assignment"
-      });
-      continue;
-    }
-
-    if (currentElement === "'") {
-      let value = "";
-      current++;
-
-      while (input[current] !== "'") {
-        value += input[current];
+        while (/[0-9]/.test(rows[i][current])) {
+          value += rows[i][current];
+          current++;
+        }
         current++;
-      }
-      current++;
 
-      tokens.push({
-        type: "string",
-        value: value
-      });
-      continue;
-    }
+        tokens.push({
+          type: "number",
+          value: parseInt(value, 10)
+        });
 
-    if (currentElement === "(") {
-      tokens.push({
-        type: "paren",
-        value: currentElement
-      });
-
-      current++;
-      continue;
-    }
-
-    if (currentElement === ")") {
-      tokens.push({
-        type: "paren",
-        value: currentElement
-      });
-
-      current++;
-      continue;
-    }
-
-    const characters = /[a-z\.]/i;
-    if (characters.test(currentElement)) {
-      let value = currentElement;
-
-      while (characters.test(input[++current])) {
-        value += input[current];
+        continue;
       }
 
-      tokens.push({
-        type: "name",
-        value: value
-      });
+      if (currentElement === "$") {
+        let variableName = "";
 
-      continue;
+        while (/[a-z]/i.test(rows[i][++current])) {
+          if (current >= colLength) {
+            throw new TypeError("Unexpected end of input in row " + (i + 1));
+          }
+
+          variableName += rows[i][current];
+        }
+
+        tokens.push({
+          type: "variable",
+          value: variableName
+        });
+
+        continue;
+      }
+
+      if (currentElement === "=") {
+        current++;
+
+        tokens.push({
+          type: "assignment"
+        });
+
+        continue;
+      }
+
+      if (currentElement === "'") {
+        let value = "";
+
+        while (rows[i][++current] !== "'") {
+          if (current >= colLength) {
+            throw new SyntaxError("Expecting a closing ' in row " + (i + 1));
+          }
+
+          value += rows[i][current];
+        }
+        current++;
+
+        tokens.push({
+          type: "string",
+          value: value
+        });
+        continue;
+      }
+
+      if (currentElement === "(") {
+        current++;
+
+        tokens.push({
+          type: "paren",
+          value: currentElement
+        });
+
+        continue;
+      }
+
+      if (currentElement === ")") {
+        current++;
+
+        tokens.push({
+          type: "paren",
+          value: currentElement
+        });
+
+        continue;
+      }
+
+      const characters = /[a-z\.]/i;
+      if (characters.test(currentElement)) {
+        let value = currentElement;
+
+        while (characters.test(rows[i][++current])) {
+          value += rows[i][current];
+        }
+        current++;
+
+        tokens.push({
+          type: "name",
+          value: value
+        });
+
+        continue;
+      }
+
+      throw new TypeError("Invalid symbol " + rows[i][current] + " given in row: " + (i + 1));
     }
   }
-
+  console.error(tokens);
   return tokens;
 }
 
--- src/tokenize.js
+++ src/tokenize.js
@@ -1,51 +1,67 @@
 export default function tokenize(input) {
-  let current = 0;
   const tokens = [];
-  
-  while(current < input.length) {
-    const currentElement = input[current];
-    
-    if(currentElement === "#") {
-      current++;
-      if(input[current] === "#") {
+
+  const rows = input.split("\n");
+  let isComment = false;
+
+  const rowsLength = rows.length;
+  for(let i = 0; i < rowsLength; i++) {
+
+    const colLength = rows[i].length;
+    let current = 0;
+    while(current < colLength) {
+      const currentElement = rows[i][current];
+
+      if(isComment && currentElement !== "#") {
         current++;
-        while(input[current++] !== "#") {}
-      } else {
-        while(input[current++] !== "\n") {}
-      }
-      
-      continue;
-    }
-    
-    if(/\s/.test(currentElement)) {
-      current++;
-      continue;
-    }
-    
-    if(/[0-9]/.test(currentElement)) {
-      let value = "";
-      
-      while(/[0-9]/.test(input[current])) {
-        value += input[current];
+        continue;
+      } else if(isComment && currentElement === "#") {
+        isComment = false;
         current++;
+        continue;
+      }  
+
+      if(currentElement === "#") {
+        if(rows[i][++current] === "#") {
+          current++;
+          isComment = true;
+          continue;
+        }
+        
+        break;
       }
-      
-      tokens.push({
-        type: "number",
-        value: parseInt(value, 10)
-      });
-      
-      continue;
-    }
-    
+
+      if(/\s/.test(currentElement)) {
+        current++;
+        continue;
+      }
+
+      if(/[0-9]/.test(currentElement)) {
+        let value = "";
+        
+        while(/[0-9]/.test(rows[i][current])) {
+          value += rows[i][current];
+          current++;
+        }
+        current++;
+        
+        tokens.push({
+          type: "number",
+          value: parseInt(value, 10)
+        });
+        
+        continue;
+      }
+
     if(currentElement === "$") {
-       current++;
-      
       let variableName = "";
       
-      while(/[a-z]/i.test(input[current])) {
-        variableName += input[current];
-        current++;
+      while(/[a-z]/i.test(rows[i][++current])) {
+        if(current >= colLength) {
+          throw new TypeError("Unexpected end of input in row " + (i + 1));
+        }
+
+        variableName += rows[i][current];
       }
       
       tokens.push({
@@ -58,20 +74,23 @@ export default function tokenize(input) {
     
     if(currentElement === "=") {
       current++;
-      
+
       tokens.push({
         type: "assignment"
       });
+
       continue;
     }
     
     if(currentElement === "'") {
       let value = "";
-      current++;
       
-      while(input[current] !== "'") {
-        value += input[current];
-        current++;
+      while(rows[i][++current] !== "'") {
+        if(current >= colLength) {
+          throw new SyntaxError("Expecting a closing ' in row " + (i + 1));
+        }
+
+        value += rows[i][current];
       }
       current++;
       
@@ -83,22 +102,24 @@ export default function tokenize(input) {
     }
     
     if(currentElement === "(") {
+      current++;
+
       tokens.push({
         type: "paren",
         value: currentElement
       });
       
-      current++;
       continue;
     }
     
     if(currentElement === ")") {
+      current++;
+
       tokens.push({
         type: "paren",
         value: currentElement
       });
       
-      current++;
       continue;
     }
     
@@ -106,9 +127,10 @@ export default function tokenize(input) {
     if(characters.test(currentElement)) {
       let value = currentElement;
       
-      while(characters.test(input[++current])) {
-        value += input[current];
+      while(characters.test(rows[i][++current])) {
+        value += rows[i][current];
       }
+      current++;
       
       tokens.push({
         type: "name",
@@ -117,7 +139,10 @@ export default function tokenize(input) {
       
       continue;
     }
+
+    throw new TypeError("Invalid symbol " + rows[i][current] + " given in row: " + (i + 1));
+    }
   }
-  
+  console.error(tokens)
   return tokens;
 }
