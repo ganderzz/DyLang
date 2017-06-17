1a9022743eceb00ca9c77d54261db322042d65ba
ganderzz <stolen3@gmail.com>
2017-06-17 10:32:46 -0700

    Minify build & update rEaDmE

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,28 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
+import { minify } from "uglify-js";
+
+const packageVersion = require("./package.json").version || "";
 
 export default {
   entry: "src/index.js",
-  format: "iife",
+  format: "umd",
+  banner: "/* @dylang " + packageVersion + " */",
   plugins: [
     resolve(),
     babel({
       exclude: "node_modules/**"
-    })
+    }),
+    uglify({
+      output: {
+        comments: function(node, comment) {
+            if (comment.type === "comment2") {
+                return comment.value.indexOf("@dylang") > -1;
+            }
+        }
+      }
+    }, minify)
   ],
   moduleName: "dylang",
   dest: "./build/dylang.js"
