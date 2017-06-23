23551a50a13866024c11fca88059a06716a0b0f2
ganderzz <stolen3@gmail.com>
2017-06-22 19:54:19 -0700

    Fix

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Not building on pull requests"
+    exit 0
+fi
+
+REPO=`git config remote.origin.url`
+
+if [ -d out ]; then
+    pushd out
+    git pull origin master
+    popd
+else
+    git clone $REPO out
+fi
+
+
+pushd out
+
 npm run build
-git add -A
-git commit -m "Update Docs"
-git push origin master
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+if ! git diff-index --quiet HEAD --; then
+    git add -A .
+    git commit -m "Updating Docs"
+    git push origin master
+fi
