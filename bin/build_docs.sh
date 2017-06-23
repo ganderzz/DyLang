308be9604227c566d7a663781bfdd3dd7d24a662
ganderzz <stolen3@gmail.com>
2017-06-22 21:31:32 -0700

    Fix build

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -9,11 +9,12 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 
 git pull --rebase origin gh-pages
-git fetch
-git branch
 git checkout gh-pages
 
 npm run build
+
+find . ! -name 'docs' -type d -exec rm -fr {} +
+mv -v ./docs ./
 
 git add .
 git commit -m "Updating Docs"
