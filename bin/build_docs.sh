79111ce6a3f1dc9c71fd35ad054f14acbdbcef6a
ganderzz <stolen3@gmail.com>
2017-06-22 21:07:24 -0700

    Add pages branch

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -6,7 +6,8 @@ git config --global user.name "Travis-CI"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-git pull origin gh-pages
+git fetch
+git branch
 git checkout gh-pages
 
 npm run build
