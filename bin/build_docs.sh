ea4c856161054ed3e03c80a2037c93b199dc4c49
ganderzz <stolen3@gmail.com>
2017-06-22 21:15:26 -0700

    Add pages branch

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -6,14 +6,10 @@ git config --global user.name "Travis-CI"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-git branch
-git pull origin gh-pages --rebase
-git branch
+git pull --rebase origin gh-pages
 git checkout gh-pages
 
 npm run build
-
-ls
 
 git add .
 git commit -m "Updating Docs"
