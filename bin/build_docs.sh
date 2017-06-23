dc2a3d6d37484b3503de74b79e524cfdd33169b6
ganderzz <stolen3@gmail.com>
2017-06-22 20:10:26 -0700

    Fix

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -2,23 +2,7 @@
 
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "Not building on pull requests"
-    exit 0
-fi
-
 REPO=`git config remote.origin.url`
-
-if [ -d out ]; then
-    pushd out
-    git pull origin master
-    popd
-else
-    git clone $REPO out
-fi
-
-
-pushd out
 
 npm run build
 
