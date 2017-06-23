4f1e6cf3da4a4b0d161a051b7b83be707700067c
ganderzz <stolen3@gmail.com>
2017-06-22 20:15:59 -0700

    Add Deployment

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -6,9 +6,6 @@ REPO=`git config remote.origin.url`
 
 npm run build
 
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
 git add -A .
 git commit -m "Updating Docs"
 git push origin master
