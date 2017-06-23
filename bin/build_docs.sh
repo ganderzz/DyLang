75e0caf8ddfb32aa13df1bfe167b7d358992a71b
ganderzz <stolen3@gmail.com>
2017-06-22 21:25:39 -0700

    Fix build

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -4,7 +4,9 @@ set -e
 
 git config --global user.name "Travis-CI"
 git config credential.helper "store --file=.git/credentials"
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
 
 git pull --rebase origin gh-pages
 git fetch
