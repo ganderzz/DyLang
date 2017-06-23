b931717bc57073af4d892366621074783463bf55
ganderzz <stolen3@gmail.com>
2017-06-22 20:40:21 -0700

    Fix

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -7,6 +7,8 @@ git config --global user.name "Travis-CI"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
+git checkout origin master
+
 npm run build
 
 git add -A .
