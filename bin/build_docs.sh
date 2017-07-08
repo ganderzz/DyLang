23f4c5d11ab8657127a1737f087431cf32200364
ganderzz <stolen3@gmail.com>
2017-07-08 09:21:10 -0700

    Build

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -3,16 +3,23 @@
 set -e
 
 pushd ./docs
-git init
-git remote add origin https://github.com/ganderzz/DyLang.git
-git fetch
-git checkout -t origin/gh-pages || exit 0;
 
-git config --global user.name "Travis-CI"
-git config --global user.email "Travis@ci.com"
-git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+branch = `git rev-parse --abbrev-ref HEAD`
+
+if [branch -e "gh-pages"]; then
+    git pull origin gh-pages
+else
+    git init
+    git remote add origin https://github.com/ganderzz/DyLang.git
+    git fetch
+    git checkout -t origin/gh-pages || exit 0;
+
+    git config --global user.name "Travis-CI"
+    git config --global user.email "Travis@ci.com"
+    git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+    git config credential.helper "store --file=.git/credentials"
+    echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+fi
 
 popd
 npm run build
