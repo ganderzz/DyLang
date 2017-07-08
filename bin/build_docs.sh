5417d0d0384aada07d91d3ebbbe187c31f0e2c2a
ganderzz <stolen3@gmail.com>
2017-07-08 10:44:22 -0700

    Build

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -8,7 +8,6 @@ pushd ../docs
 git init
 git remote add origin https://github.com/ganderzz/DyLang.git
 git fetch
-git branch
 git checkout -t origin/gh-pages || exit 0;
 
 git config --global user.name "Travis-CI"
@@ -19,10 +18,11 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 popd
 npm run build
+rm ../docs/*
+cp ./docs ../docs
 pushd ../docs
 
 git add .
 git commit -m "Updating Docs"
 
-git branch
-git push --force --quiet
+git push --force --quiet || exit 0;
